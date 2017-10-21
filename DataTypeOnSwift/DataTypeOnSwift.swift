6cdefa45391d586e36384c561c3fe1329c834e62
practica <practica@i9950.local>
2017-10-21 14:52:45 -0500

    modificado conflictos JAIRO

--- DataTypeOnSwift/DataTypeOnSwift.swift
+++ DataTypeOnSwift/DataTypeOnSwift.swift
@@ -4,7 +4,7 @@ let    autosEnLaCochera   =    2
 let    estrellasEnElCielo   =    1_000_000     //   Puedes   usar   _   entre   números
 
 
-let decimal   =    17
+let decimal   =    21
 let binario   =    0b10001                       //   17   en   notación   binaria
 let octal   = 0o21
 let  hexadecimal   =    0x11
@@ -14,5 +14,8 @@ let suma   =   decimal   +   octal       //   #=>   34
 
 //   Float
 let    gramos:    Float    =    85.32
+
+// cambios
+
 
 
