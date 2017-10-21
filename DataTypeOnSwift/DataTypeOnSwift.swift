edfcef634d5cf328e8b5a4b5264b35851733dce5
julian pardo <hectorj.pardom@utadeo.edu.co>
2017-10-21 14:44:08 -0500

    Modificado lineas -Julian

--- DataTypeOnSwift/DataTypeOnSwift.swift
+++ DataTypeOnSwift/DataTypeOnSwift.swift
@@ -2,3 +2,16 @@
 let    dedosEnLaMano   =    5
 let    autosEnLaCochera   =    2
 let    estrellasEnElCielo   =    1_000_000     //   Puedes   usar   _   entre   números
+
+
+let decimal   =    17
+let binario   =    0b10001                       //   17   en   notación   binaria
+let octal   = 0o21
+let  hexadecimal   =    0x11
+
+let suma   =   decimal   +   octal       //   #=>   34
+
+
+
+
+
